407655e03e706f029a57ca0d7424f9d618ff1603
nononoooooo <66546598+nononoooooo@users.noreply.github.com>
2020-06-11 22:22:45 +0900

    Update sample.py

--- source/sample.py
+++ source/sample.py
@@ -3,7 +3,6 @@ import csv
 import numpy as np
 
 with open("./dataset/pokemon_status.csv", encoding='utf-8') as csv_file:
-
     csv_data =[(row["図鑑番号"], row["ポケモン名"], row["タイプ１"], row["タイプ２"], row["合計"]) 
         for row in csv.DictReader(csv_file) if not "-" in row["図鑑番号"] and row["合計"].isdigit()]
 
@@ -14,6 +13,5 @@ min_total_index = np.argmin([col[4] for col in csv_data])
 print("最小:{}",csv_data[min_total_index][1])
 
 # get unique type
-
-type1 = set(set([col[2] for col in csv_data]) & set([col[2] for col in csv_data]))
-print(type1)
+type_ = set(set([col[2] for col in csv_data]) & set([col[3] for col in csv_data]))
+print(type_)
